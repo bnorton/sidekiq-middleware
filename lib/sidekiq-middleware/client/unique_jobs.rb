46aff848c325b8809a7e450bb7630ce05472e1df
Brian Norton <brian.nort@gmail.com>
2012-09-26 13:14:41 -0700

    Remove the sent_at key from the payload.

--- lib/sidekiq-middleware/client/unique_jobs.rb
+++ lib/sidekiq-middleware/client/unique_jobs.rb
@@ -8,19 +8,19 @@ module Sidekiq
           enabled = worker_class.get_sidekiq_options['unique']
 
           if enabled
-            unique = false
+            unique, payload = false, item.clone
+
+            payload.delete('jid')
+            payload.delete('sent_at')
 
             # Enabled unique scheduled
             if enabled == :all && item.has_key?('at')
               expiration = worker_class.get_sidekiq_options['expiration'] || (item['at'].to_i - Time.new.to_i)
-              payload = item.clone
               payload.delete('at')
-              payload.delete('jid')
             else
               expiration = worker_class.get_sidekiq_options['expiration'] || HASH_KEY_EXPIRATION
-              payload = item.clone
-              payload.delete('jid')
             end
+
             payload_hash = Digest::MD5.hexdigest(Sidekiq.dump_json(Hash[payload.sort]))
 
             Sidekiq.redis do |conn|
--- lib/sidekiq-middleware/server/unique_jobs.rb
+++ lib/sidekiq-middleware/server/unique_jobs.rb
@@ -18,17 +18,16 @@ module Sidekiq
         end
 
         def clear(worker_instance, item, queue)
-          enabled = worker_instance.class.get_sidekiq_options['unique']
+          enabled, payload = worker_instance.class.get_sidekiq_options['unique'], item.clone
+
+          payload.delete('jid')
+          payload.delete('sent_at')
 
           # Enabled unique scheduled 
           if enabled == :all && item.has_key?('at')
-            payload = item.clone
             payload.delete('at')
-            payload.delete('jid')
-          else
-            payload = item.clone
-            payload.delete('jid')
           end
+
           payload_hash = Digest::MD5.hexdigest(Sidekiq.dump_json(Hash[payload.sort]))
           
           Sidekiq.redis { |conn| conn.del(payload_hash) }
